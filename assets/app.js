0f4454d6bb90df4119aa203e09b3098429e6999d
Emilio T <te.emilio@gmail.com>
2013-06-27 15:13:39 -0400

    Fixed the link I missed in the javascript

--- assets/app.js
+++ assets/app.js
@@ -1,8 +1,8 @@
 (function() {
 
-    $('#api_group').on('change', function() { $('#example').load('/partials/' + $(this).val() + '.html'); });
+    $('#api_group').on('change', function() { $('#example').load('/node-stripe-api/partials/' + $(this).val() + '.html'); });
 
-    $('#example').load('/partials/' + $('#api_group').val() + '.html');
+    $('#example').load('/node-stripe-api/partials/' + $('#api_group').val() + '.html');
 
 })();
 
